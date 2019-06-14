9cb6d23b779ca1634e2da7a4d30dc4f673f443e3
Fredrik Trusohamn <48571579+Uikikr@users.noreply.github.com>
2019-06-14 21:51:08 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let fredrik = {
+    name: 'Fredrik',
+    lastName: 'Trusohamn',
+    email: 'fredrik.trusohamn@gmail.com'    
+};
+
+function logUserInfo(user) {
+    
+console.log(user.name + ':' + user.lastName + ':' +  user.email)
+    
+};
+
+logUserInfo(fredrik)
