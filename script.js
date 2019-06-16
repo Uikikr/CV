1b2e07a8e7396f779d543ec1532aa4d9134a66a0
Fredrik Trusohamn <48571579+Uikikr@users.noreply.github.com>
2019-06-17 00:16:10 +0200

    changes to layout

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
-let fredrik = {
-    name: 'Fredrik',
-    lastName: 'Trusohamn',
-    email: 'fredrik.trusohamn@gmail.com',
-    phone: '+46 76 001 96 30'    
-};
+    let fredrik = {
+        name: 'Fredrik',
+        lastName: 'Trusohamn',
+        email: 'fredrik.trusohamn@gmail.com',
+        phone: '+46 76 001 96 30'    
+    };
+    
+    function logUserInfo(user) {
+    console.log(user.name + ':' + user.lastName + ':' +  user.email + ':' + user.phone)  
+    };
+    
+    logUserInfo(fredrik)
+    
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+    }
 
-function logUserInfo(user) {
-console.log(user.name + ':' + user.lastName + ':' +  user.email + ':' + user.phone)  
-};
+let modal = document.getElementById("About_Modal");
 
-logUserInfo(fredrik)
+let btn = document.getElementById("About");
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
