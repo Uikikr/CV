cdbb0872258b10de280347ed263f5416c5f02146
Fredrik Trusohamn <48571579+Uikikr@users.noreply.github.com>
2019-06-17 22:58:58 +0200

    Edits to html, css and java

--- script.js
+++ script.js
@@ -1,55 +1,50 @@
-    let fredrik = {
-        name: 'Fredrik',
-        lastName: 'Trusohamn',
-        email: 'fredrik.trusohamn@gmail.com',
-        phone: '+46 76 001 96 30'    
-    };
-    
-    function logUserInfo(user) {
-    console.log(user.name + ':' + user.lastName + ':' +  user.email + ':' + user.phone)  
-    };
-    
-    logUserInfo(fredrik)
+let fredrik = {
+  name: 'Fredrik',
+  lastName: 'Trusohamn',
+  email: 'fredrik.trusohamn@gmail.com',
+  phone: '+46 76 001 96 30'
+};
 
-    function menyDrop() {
-      var x = document.getElementById("nav_link");
-      if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
+function logUserInfo(user) {
+  console.log(user.name + ':' + user.lastName + ':' + user.email + ':' + user.phone)
+};
+
+logUserInfo(fredrik)
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function dropDownF() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  };
+  if (!event.target.matches('.dropBttn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-    
-    let slideIndex = 1;
-    showSlides(slideIndex);
-    
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
-    
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
-    
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("mySlides");
-      let dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {slideIndex = 1}    
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";  
-      }
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";  
-      dots[slideIndex-1].className += " active";
-    }
+  }
+}
 
 let modal = document.getElementById("About_Modal");
 
-let btn = document.getElementById("About");
+let btn = document.getElementById("about");
 
 let span = document.getElementsByClassName("close")[0];
 
@@ -61,8 +56,29 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
